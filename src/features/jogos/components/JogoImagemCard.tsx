f802272c096dbab235e98a1f00fa0fea806741bc
Ruan Sousa <ruansousad4c@gmail.com>
2025-09-25 21:36:27 -0300

    feat: adiciona placeholder para imagens de cards caso a imagem original não seja carregada

--- src/features/jogos/components/JogoImagemCard.tsx
+++ src/features/jogos/components/JogoImagemCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "../styles/JogoCard.module.css";
 
 interface JogoImagemProps {
@@ -5,5 +6,19 @@ interface JogoImagemProps {
   alt: string;
 }
 export default function JogoImagemCard({ src, alt }: JogoImagemProps) {
-  return <img src={src} alt={alt} className={styles.jogoImagemCard}></img>;
+  const [imageSrc, setImageSrc] = useState(src);
+  return (
+    <img
+      src={imageSrc}
+      alt={alt}
+      onError={(e) =>
+        setImageSrc(
+          `data:image/svg+xml;base64,${btoa(
+            `<svg xmlns="http://www.w3.org/2000/svg" width="460" height="215"><rect width="100%" height="100%" fill="#f7f7f7"/></svg>`
+          )}`
+        )
+      }
+      className={styles.jogoImagemCard}
+    ></img>
+  );
 }
